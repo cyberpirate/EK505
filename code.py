ffbf4c85c7cdad7eb15f6fc4e937dc4b4026333f
cyberpirate <joe.v.iacoviello@gmail.com>
2020-12-03 19:18:43 -0500

    grabber added

--- code.py
+++ code.py
@@ -5,6 +5,9 @@ import math
 
 ARM_RAD = 1
 ARM_LENGTH = 4
+GRAB_MIN = 0.25
+GRAB_MAX = 1
+GRAB_LEN = 1
 
 T1 = 0
 T2 = 0
@@ -19,6 +22,8 @@ D = 0
 ARM1 = arrow(color=color.green)
 ARM2 = arrow(color=color.red)
 ARM3 = arrow(color=color.blue)
+GRAB_BAR1 = arrow(color=color.white)
+GRAB_BAR2 = arrow(color=color.white)
 
 # testArrow = arrow(color=color.red)
 
@@ -26,6 +31,12 @@ def originAxis(l=1):
     arrow(axis=(vec(l, 0, 0)), color=color.red)
     arrow(axis=(vec(0, l, 0)), color=color.green)
     arrow(axis=(vec(0, 0, l)), color=color.blue)
+
+def to01(i):
+    return (1-cos(i))/2
+
+def scale(x, bot, top):
+    return bot + (top - bot) * x
 
 def forwardKinematics(T1, T2, T3, D):
 
@@ -41,15 +52,17 @@ def forwardKinematics(T1, T2, T3, D):
     ARM3_pos = ARM2_pos + ARM2_fwd
     ARM3_fwd = ARM2_fwd.rotate(T3, ARM2_fwd.cross(vec(0, 1, 0)))
 
-    return ARM1_pos, ARM1_fwd, ARM2_pos, ARM2_fwd, ARM3_pos, ARM3_fwd
+    armbar = ARM3_fwd.cross(vec(0, 1, 0)).norm() * scale(D, GRAB_MIN, GRAB_MAX)
+
+    GRAB_BAR1_pos = ARM3_pos + ARM3_fwd + armbar
+    GRAB_BAR1_fwd = ARM3_fwd.norm() * GRAB_LEN
+
+    GRAB_BAR2_pos = ARM3_pos + ARM3_fwd - armbar
+    GRAB_BAR2_fwd = ARM3_fwd.norm() * GRAB_LEN
+    
+    return ARM1_pos, ARM1_fwd, ARM2_pos, ARM2_fwd, ARM3_pos, ARM3_fwd, GRAB_BAR1_pos, GRAB_BAR1_fwd, GRAB_BAR2_pos, GRAB_BAR2_fwd
 
 # originAxis(10)
-
-def to01(i):
-    return (1-cos(i))/2
-
-def scale(x, bot, top):
-    return bot + (top - bot) * x
 
 i = 0
 
@@ -59,9 +72,9 @@ while True:
     T1 = scale(to01(i/100), 0, 2*math.pi)
     T2 = scale(to01(i/200), -math.pi/2, math.pi/2)
     T3 = scale(to01(i/100), -math.pi/2, math.pi/2)
+    D  = to01(i/100)
 
-    # armState = forwardKinematics(T1, T2, T3, 0)
-    armState = forwardKinematics(T1, -math.pi/4, T3, 0)
+    armState = forwardKinematics(0, 0, 0, D)
         
     ARM1.pos  = armState[0]
     ARM1.axis = armState[1]
@@ -71,6 +84,12 @@ while True:
 
     ARM3.pos  = armState[4]
     ARM3.axis = armState[5]
+
+    GRAB_BAR1.pos = armState[6]
+    GRAB_BAR1.axis = armState[7]
+
+    GRAB_BAR2.pos = armState[8]
+    GRAB_BAR2.axis = armState[9]
 
     # ARM1.up = vec(-1, 0, 0)
     i += 1
