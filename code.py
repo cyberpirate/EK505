4bd5f3e29f15b8b0d8f5e27e702d2a5b803df7e8
cyberpirate <joe.v.iacoviello@gmail.com>
2020-12-04 14:02:41 -0500

    can follow a path in AStar

--- code.py
+++ code.py
@@ -2,17 +2,30 @@
 
 from vpython import *
 import math
+import numpy as np
+import AStar
+
+cspaceLim = (
+    (0, 2*math.pi),
+    (-math.pi/2, math.pi/2),
+    (-math.pi/2, math.pi/2),
+    (0, 1)
+)
+
+cspaceSegs = (
+    200,
+    100,
+    100,
+    100
+)
+
+cspace = np.ones(cspaceSegs, np.bool)
 
 ARM_RAD = 1
 ARM_LENGTH = 4
 GRAB_MIN = 0.25
 GRAB_MAX = 1
 GRAB_LEN = 1
-
-T1 = 0
-T2 = 0
-T3 = 0
-D = 0
 
 # ARM1 = cylinder(length=ARM_LENGTH, radius=ARM_RAD, color=color.green)
 # ARM2 = cylinder(length=ARM_LENGTH, radius=ARM_RAD, color=color.red)
@@ -25,7 +38,114 @@ ARM3 = arrow(color=color.blue)
 GRAB_BAR1 = arrow(color=color.white)
 GRAB_BAR2 = arrow(color=color.white)
 
-# testArrow = arrow(color=color.red)
+def cspaceToState(T1I, T2I, T3I, DI):
+
+    T1_step = (cspaceLim[0][1] - cspaceLim[0][0]) / cspaceSegs[0]
+    T2_step = (cspaceLim[1][1] - cspaceLim[1][0]) / cspaceSegs[1]
+    T3_step = (cspaceLim[2][1] - cspaceLim[2][0]) / cspaceSegs[2]
+    D_step  = (cspaceLim[3][1] - cspaceLim[3][0]) / cspaceSegs[3]
+
+    return (
+        cspaceLim[0][0] + T1_step*T1I,
+        cspaceLim[1][0] + T2_step*T2I,
+        cspaceLim[2][0] + T3_step*T3I,
+        cspaceLim[3][0] + D_step*DI
+    )
+
+def stateToCspace(T1, T2, T3, D):
+
+    T1_step = (cspaceLim[0][1] - cspaceLim[0][0]) / cspaceSegs[0]
+    T2_step = (cspaceLim[1][1] - cspaceLim[1][0]) / cspaceSegs[1]
+    T3_step = (cspaceLim[2][1] - cspaceLim[2][0]) / cspaceSegs[2]
+    D_step  = (cspaceLim[3][1] - cspaceLim[3][0]) / cspaceSegs[3]
+
+    return (
+        math.floor((T1 - cspaceLim[0][0]) / T1_step),
+        math.floor((T2 - cspaceLim[1][0]) / T2_step),
+        math.floor((T3 - cspaceLim[2][0]) / T3_step),
+        math.floor((D  - cspaceLim[3][0]) / D_step)
+    )
+
+class CSpaceNode(AStar.AStarNode):
+
+    def __init__(self, T1I, T2I, T3I, DI, nodeFilters):
+        self.T1I = T1I
+        self.T2I = T2I
+        self.T3I = T3I
+        self.DI = DI
+        self.nodeFilters = nodeFilters
+
+    def __eq__(self, other):
+
+        if not hasattr(other, "T1I") or self.T1I != other.T1I:
+            return False
+
+        if not hasattr(other, "T2I") or self.T2I != other.T2I:
+            return False
+
+        if not hasattr(other, "T3I") or self.T3I != other.T3I:
+            return False
+
+        if not hasattr(other, "DI") or self.DI != other.DI:
+            return False
+
+        return True
+    
+    def __str__(self):
+        return "({}, {}, {}, {})".format(*cspaceToState(self.T1I, self.T2I, self.T3I, self.DI))
+    
+    def __repr__(self):
+        return "CSpaceNode{}".format(str(self))
+
+    def validNode(self):
+
+        for nodeFilter in self.nodeFilters:
+            if not nodeFilter(self.T1I, self.T2I, self.T3I, self.DI):
+                return False
+
+        return True
+
+
+    def adj(self):
+
+        ret = [
+            CSpaceNode(self.T1I+1, self.T2I, self.T3I, self.DI, self.nodeFilters),
+            CSpaceNode(self.T1I-1, self.T2I, self.T3I, self.DI, self.nodeFilters),
+            CSpaceNode(self.T1I, self.T2I+1, self.T3I, self.DI, self.nodeFilters),
+            CSpaceNode(self.T1I, self.T2I-1, self.T3I, self.DI, self.nodeFilters),
+            CSpaceNode(self.T1I, self.T2I, self.T3I+1, self.DI, self.nodeFilters),
+            CSpaceNode(self.T1I, self.T2I, self.T3I-1, self.DI, self.nodeFilters),
+            CSpaceNode(self.T1I, self.T2I, self.T3I, self.DI+1, self.nodeFilters),
+            CSpaceNode(self.T1I, self.T2I, self.T3I, self.DI-1, self.nodeFilters)
+        ]
+
+        ret = [node for node in ret if self.validNode()]
+
+        return ret
+
+    def estCost(self, node):
+
+        T1I_cost = abs(self.T1I - node.T1I)
+        T2I_cost = abs(self.T2I - node.T2I)
+        T3I_cost = abs(self.T3I - node.T3I)
+        DI_cost  = abs(self.DI  - node.DI)
+
+        return T1I_cost + T2I_cost + T3I_cost + DI_cost
+
+    def cost(self, node):
+        if self.estCost(node) != 1:
+            raise RuntimeError("Node not adjacent")
+        return 1
+
+def followPath(path):
+
+    for node in path:
+        rate(30)
+
+        state = cspaceToState(node.T1I, node.T2I, node.T3I, node.DI)
+        renderForwardKinematics(*state)
+
+
 
 def originAxis(l=1):
     arrow(axis=(vec(l, 0, 0)), color=color.red)
@@ -60,21 +180,16 @@ def forwardKinematics(T1, T2, T3, D):
     GRAB_BAR2_pos = ARM3_pos + ARM3_fwd - armbar
     GRAB_BAR2_fwd = ARM3_fwd.norm() * GRAB_LEN
     
-    return ARM1_pos, ARM1_fwd, ARM2_pos, ARM2_fwd, ARM3_pos, ARM3_fwd, GRAB_BAR1_pos, GRAB_BAR1_fwd, GRAB_BAR2_pos, GRAB_BAR2_fwd
+    return (
+        ARM1_pos, ARM1_fwd,
+        ARM2_pos, ARM2_fwd,
+        ARM3_pos, ARM3_fwd,
+        GRAB_BAR1_pos, GRAB_BAR1_fwd,
+        GRAB_BAR2_pos, GRAB_BAR2_fwd
+    )
 
-# originAxis(10)
-
-i = 0
-
-while True:
-    rate(60)
-
-    T1 = scale(to01(i/100), 0, 2*math.pi)
-    T2 = scale(to01(i/200), -math.pi/2, math.pi/2)
-    T3 = scale(to01(i/100), -math.pi/2, math.pi/2)
-    D  = to01(i/100)
-
-    armState = forwardKinematics(0, 0, 0, D)
+def renderForwardKinematics(T1, T2, T3, D):
+    armState = forwardKinematics(T1, T2, T3, D)
         
     ARM1.pos  = armState[0]
     ARM1.axis = armState[1]
@@ -91,5 +206,47 @@ while True:
     GRAB_BAR2.pos = armState[8]
     GRAB_BAR2.axis = armState[9]
 
-    # ARM1.up = vec(-1, 0, 0)
-    i += 1
+# originAxis(10)
+
+renderForwardKinematics(0, 0, 0, 0)
+
+startCspace = stateToCspace(0, 0, 0, 0)
+endCspace = stateToCspace(math.pi, math.pi/2, -math.pi/2, 1)
+
+startNode = CSpaceNode(*startCspace, [])
+endNode = CSpaceNode(*endCspace, [])
+
+path = AStar.AStar(startNode, endNode)
+
+i = 0
+
+while True:
+
+    followPath(path)
+
+    # rate(60)
+
+    # T1 = scale(to01(i/100), 0, 2*math.pi)
+    # T2 = scale(to01(i/200), -math.pi/2, math.pi/2)
+    # T3 = scale(to01(i/100), -math.pi/2, math.pi/2)
+    # D  = to01(i/100)
+
+    # armState = forwardKinematics(0, 0, 0, D)
+        
+    # ARM1.pos  = armState[0]
+    # ARM1.axis = armState[1]
+
+    # ARM2.pos  = armState[2]
+    # ARM2.axis = armState[3]
+
+    # ARM3.pos  = armState[4]
+    # ARM3.axis = armState[5]
+
+    # GRAB_BAR1.pos = armState[6]
+    # GRAB_BAR1.axis = armState[7]
+
+    # GRAB_BAR2.pos = armState[8]
+    # GRAB_BAR2.axis = armState[9]
+
+    # # ARM1.up = vec(-1, 0, 0)
+    # i += 1
