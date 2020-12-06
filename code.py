c5f486dec4f2f48be88b77e5137bcdc9fd50be26
cyberpirate <joe.v.iacoviello@gmail.com>
2020-12-06 13:26:52 -0500

    inverse works

--- code.py
+++ code.py
@@ -132,31 +132,143 @@ def renderForwardKinematics(T1, T2, T3, D):
         attachedObs.pos = ARM3.pos + ARM3.axis
         attachedObs.pos += ARM3.axis.norm() * (GRAB_LEN - attachedObs.radius)
 
+def link2ForwardKinematics(T1, T2, L1, L2):
+    x1 = L1*math.cos(T1)
+    y1 = L1*math.sin(T1)
+
+    origin = vec(0, 0, 0)
+    mid = vec(x1, y1, 0)
+    end = vec(x1 + L2*math.cos(T1 + T2), y1 + L2*math.sin(T1 + T2), 0)
+
+    return (
+        origin, mid,
+        mid, end - mid,
+        end
+    )
+
+def link2CheckInv(T1, T2, L1, L2, pos):
+    ans = link2ForwardKinematics(T1, T2, L1, L2)[4]
+    return (ans - pos).mag < 0.01
+
+def link2InverseKinematics(x, y, L1, L2):
+    cT2 = (x**2 + y**2 - L1**2 - L2**2) / (2*L1*L2)
+    sT2_pos = math.sqrt(1 - cT2**2)
+    sT2_neg = -sT2_pos
+
+    T2_pos = math.atan2(sT2_pos, cT2)
+    T2_neg = math.atan2(sT2_neg, cT2)
+
+    cT1_pos = (x*(L1 + L2*cT2) + y*L2*sT2_pos) / (x**2 + y**2)
+    cT1_neg = (x*(L1 + L2*cT2) + y*L2*sT2_neg) / (x**2 + y**2)
+
+    sT1_pos = math.sqrt(1 - cT1_pos**2)
+    sT1_neg = -sT1_pos
+
+    T1_pos = math.atan2(sT1_pos, cT1_pos)
+    T1_neg = math.atan2(sT1_neg, cT1_pos)
+
+    sT1_pos2 = math.sqrt(1 - cT1_neg**2)
+    sT1_neg2 = -sT1_pos2
+
+    T1_pos2 = math.atan2(sT1_pos2, cT1_neg)
+    T1_neg2 = math.atan2(sT1_neg2, cT1_neg)
+
+    ret = [
+        (T1_neg, T2_pos),   # color.red
+        (T1_pos, T2_pos),   # color.green
+        (T1_neg2, T2_neg),  # color.white
+        (T1_pos2, T2_neg)   # color.yellow
+    ]
+
+    ret = [ r for r in ret if link2CheckInv(*r, L1, L2, vec(x, y, 0))]
+
+    return ret
+
 def inverseKinematics(x, y, z):
 
     L1 = ARM_LENGTH
     L2 = ARM_LENGTH
     L3 = ARM_LENGTH
 
-    T1 = math.atan(y / x)
+    pp = vec(x, 0, z)
 
-    T2 = math.atan((z - L1) / math.sqrt(x**2 + y**2))
+    T1 = math.acos(pp.dot(vec(1, 0, 0)) / pp.mag)
 
-    T2_ = math.acos((L3**2 - L2**2 - x**2 - y**2 - (z - L1)**2) / (-2*L2*math.sqrt(x**2 + y**2 + (z - L1)**2)))
+    if z > 0:
+        T1 = math.pi + (math.pi - T1)
 
-    T3 = math.pi - math.acos((x**2 + y**2 + (z - L1)**2 - L2**2 - L3**2) / (-2*L2*L3))
+    xp = pp.mag
+    yp = y - L1
 
-    ret = [
-        (T1, T2 + T2_, -T3),
-        (T1, T2 - T2_, T3)
+    link2State = link2InverseKinematics(xp, yp, L2, L3)
+
+    return [
+        (T1, *link2State[0]),
+        (T1, *link2State[1])
     ]
 
-    ret = [ r for r in ret if CSpace.notOutOfBounds(*CSpace.stateToCspace(*r, 0))]
+# def inverseKinematics(x, y, z):
 
-    if len(ret) == 0:
-        raise RuntimeError("No inverse kinematics found")
+#     L1 = ARM_LENGTH
+#     L2 = ARM_LENGTH
+#     L3 = ARM_LENGTH
 
-    return ret
+#     # T1 = math.atan(y / x)
+
+#     # T2 = math.atan((z - L1) / math.sqrt(x**2 + y**2))
+
+#     # T2_ = math.acos((L3**2 - L2**2 - x**2 - y**2 - (z - L1)**2) / (-2*L2*math.sqrt(x**2 + y**2 + (z - L1)**2)))
+
+#     # T3 = math.pi - math.acos((x**2 + y**2 + (z - L1)**2 - L2**2 - L3**2) / (-2*L2*L3))
+
+#     # ret = [
+#     #     (T1, T2 + T2_, -T3),
+#     #     (T1, T2 - T2_, T3)
+#     # ]
+
+#     # ret = [ r for r in ret if CSpace.notOutOfBounds(*CSpace.stateToCspace(*r, 0))]
+
+#     # if len(ret) == 0:
+#     #     raise RuntimeError("No inverse kinematics found")
+
+#     # return ret
+
+#     # T3 = math.acos((x**2 + y**2 + (z-L1)**2 - L2**2 - L3**2) / (2*L2*L3))
+
+#     # a = L3*math.sin(T3)
+#     # b = L2 + L3*math.cos(T3)
+#     # c = z - L1
+#     # r = math.sqrt(a**2 + b**2)
+
+#     # T2 = math.atan(c / math.sqrt(r**2 - c**2)) - math.atan(a/b)
+#     # T1 = math.atan(y/x)
+
+#     # return [(T1, T2, T3)]
+
+#     pp = vec(x, 0, z)
+#     T1 = math.acos(pp.dot(vec(1, 0, 0)) / pp.mag)
+
+#     planeNorm = pp.cross(vec(0, 1, 0)).norm()
+
+#     v = goal.pos - vec(0, ARM_LENGTH, 0)
+#     w = planeNorm
+#     vt = v - (v.dot(w)/w.mag**2)*w
+#     goalp = vec(0, ARM_LENGTH, 0) + vt
+
+#     xp = vec(goalp.x, 0, goalp.z).mag
+#     yp = goalp.y
+
+#     cT3 = (xp**2 + yp**2 - L2**2 - L3**2) / (2*L2*L3)
+#     sT3_pos = math.sqrt(1 - cT3**2)
+#     sT3_neg = -sT3_pos
+
+#     T3_pos = math.atan2(sT3_pos, cT3)
+#     T3_neg = math.atan2(sT3_neg, cT3)
+
+#     cT2_pos = (xp*(L2 + L2*cT3) + yp*L3*sT3_pos) / (xp**2 + yp**2)
+#     cT2_neg = (xp*(L2 + L2*cT3) + yp*L3*sT3_neg) / (xp**2 + yp**2)
+
+#     return [(0, 0, 0)]
 
 def travelAndOpen(startState, endXYZ):
     goal = inverseKinematics(*endXYZ)
@@ -199,103 +311,100 @@ def travel(startState, endState):
 
     return AStar.AStar(startNode, endNode)
 
-while True:
-    initOBS()
-    startState = (0, 0, 0, 0)
-    robotState = startState
-    renderForwardKinematics(*robotState)
-
-    for i in range(len(OBS)):
-
-        print("Moving {}".format(i))
-
-        # Travel to the object to pick up
-        path = travelAndOpen(robotState, (OBS[i].pos.x, OBS[i].pos.y, OBS[i].pos.z))
-        followPath(path)
-        robotState = path[-1].getState()
-
-        # Close around the object
-        path = closeHere(robotState)
-        followPath(path)
-        robotState = path[-1].getState()
-
-        # Set the object as "grabbed"
-        attachedObs = OBS[i]
-
-        # Drag object to where we place it
-        placePos = vec(2 - i*GRAB_MAX*1.5, ARM_LENGTH, -2)
-        path = travelKeepClosed(robotState, (placePos.x, placePos.y, placePos.z))
-        followPath(path)
-        robotState = path[-1].getState()
-
-        # Open end effector
-        path = openHere(robotState)
-        followPath(path)
-        robotState = path[-1].getState()
-
-        # Drop object
-        attachedObs = None
-
-    # back to start state
-    path = travel(robotState, startState)
-    followPath(path)
-    robotState = path[-1].getState()
-
-# print(checkNoCollision(*robotState))
-
-# for i in range(100):
-#     rate(30)
-#     robotState = (robotState[0], robotState[1] + 0.01, robotState[2], robotState[3])
-#     print(checkNoCollision(*robotState))
+# while True:
+#     initOBS()
+#     startState = (0, 0, 0, 0)
+#     robotState = startState
 #     renderForwardKinematics(*robotState)
 
-# originAxis(10)
+#     for i in range(len(OBS)):
 
-# print(inverseKinematics(-ARM_LENGTH/2, ARM_LENGTH, 0))
-# print(inverseKinematics((1 + math.sqrt(3)) / 2, 0, (3 + sqrt(3)) / 2))
-# goal = inverseKinematics(OBS[0].pos.x, OBS[0].pos.y, OBS[0].pos.z)
-# print(goal)
+#         print("Moving {}".format(i))
 
-# renderForwardKinematics(0, 0, 0, 0)
+#         # Travel to the object to pick up
+#         path = travelAndOpen(robotState, (OBS[i].pos.x, OBS[i].pos.y, OBS[i].pos.z))
+#         followPath(path)
+#         robotState = path[-1].getState()
 
-# startCspace = CSpace.stateToCspace(0, 0, 0, 0)
-# # endCspace = CSpace.stateToCspace(math.pi, math.pi/2, -math.pi/2, 1)
-# endCspace = CSpace.stateToCspace(*goal[0], 1)
+#         # Close around the object
+#         path = closeHere(robotState)
+#         followPath(path)
+#         robotState = path[-1].getState()
 
-# startNode = CSpace.CSpaceNode(*startCspace, [checkNoCollision])
-# endNode = CSpace.CSpaceNode(*endCspace, [])
+#         # Set the object as "grabbed"
+#         attachedObs = OBS[i]
 
-# path = AStar.AStar(startNode, endNode)
+#         # Drag object to where we place it
+#         placePos = vec(2 - i*GRAB_MAX*1.5, ARM_LENGTH, -2)
+#         path = travelKeepClosed(robotState, (placePos.x, placePos.y, placePos.z))
+#         followPath(path)
+#         robotState = path[-1].getState()
+
+#         # Open end effector
+#         path = openHere(robotState)
+#         followPath(path)
+#         robotState = path[-1].getState()
+
+#         # Drop object
+#         attachedObs = None
+
+#     # back to start state
+#     path = travel(robotState, startState)
+#     followPath(path)
+#     robotState = path[-1].getState()
+
+renderForwardKinematics(0, 0, 0, 0)
+
+goal = OBS[0]
+goal.pos = vec(4, 5, -4)
+
+state = inverseKinematics(goal.pos.x, goal.pos.y, goal.pos.z)
+print(state)
+
+i = 0
+while True:
+
+    rate(1)
+
+    renderForwardKinematics(*state[i % len(state)], 0)
+
+    i += 1
 
 # i = 0
 
+# orbit = OBS[0]
+# goal = OBS[1]
+# goalp = OBS[2]
+# norm = OBS[3]
+
+# goal.pos = vec(4, 5, -4)
+
+# arr = arrow()
+
 # while True:
+#     i += 1
 
-#     followPath(path)
+#     rate(30)
+    
 
-#     # rate(60)
+#     orbit.pos = vec(ARM_LENGTH, 0, 0).rotate(i/100, axis=vec(0, 1, 0))
 
-#     # T1 = scale(to01(i/100), 0, 2*math.pi)
-#     # T2 = scale(to01(i/200), -math.pi/2, math.pi/2)
-#     # T3 = scale(to01(i/100), -math.pi/2, math.pi/2)
-#     # D  = to01(i/100)
+#     pp = vec(orbit.pos.x, 0, orbit.pos.z)
 
-#     # armState = forwardKinematics(0, 0, 0, D)
-        
-#     # ARM1.pos  = armState[0]
-#     # ARM1.axis = armState[1]
+#     T1 = math.acos(pp.dot(vec(1, 0, 0)) / pp.mag)
 
-#     # ARM2.pos  = armState[2]
-#     # ARM2.axis = armState[3]
+#     if orbit.pos.z > 0:
+#         T1 = math.pi + (math.pi - T1)
 
-#     # ARM3.pos  = armState[4]
-#     # ARM3.axis = armState[5]
+#     renderForwardKinematics(T1, 0, 0, 0)
+    
 
-#     # GRAB_BAR1.pos = armState[6]
-#     # GRAB_BAR1.axis = armState[7]
+#     planeNorm = pp.cross(vec(0, 1, 0))
+#     norm.pos = planeNorm.norm()
 
-#     # GRAB_BAR2.pos = armState[8]
-#     # GRAB_BAR2.axis = armState[9]
-
-#     # # ARM1.up = vec(-1, 0, 0)
-#     # i += 1
+#     v = goal.pos - vec(0, ARM_LENGTH, 0)
+#     w = planeNorm
+#     vt = v - (v.dot(w)/w.mag**2)*w
+#     goalp.pos = vec(0, ARM_LENGTH, 0) + vt
+#     arr.pos = goalp.pos
+#     arr.axis = goal.pos - goalp.pos
